9bf2b723d6e7bc027fc381018072267e10946477
choisungwook <kgg1959@naver.com>
2023-02-10 23:04:34 +0900

    #1 리팩토링

--- app/domain/webapp/service.py
+++ app/domain/webapp/service.py
@@ -3,6 +3,7 @@ from config import Settings
 from api.models.webapp import RequestBodyCreateWebApp
 from .models import CreateJenkinsJobParameters
 
+
 def TriggerJenkinsJob(request_body: RequestBodyCreateWebApp, settings: Settings):
     """생성을 jenkins에게 위임"""
     jenkins_client = jenkins.Jenkins(settings.jenkins_url, username=settings.jenkins_username, password=settings.jenkins_password)
@@ -12,26 +13,33 @@ def TriggerJenkinsJob(request_body: RequestBodyCreateWebApp, settings: Settings)
     except IndexError:
         raise RuntimeError("유효하지 않은 템플릿type")
 
-    jenkins_job_parameters = CreateJenkinsJobParameters(
-        applicationName=request_body.applicationName,
-        templateVersion=request_body.templateVersion,
-        githubLink=request_body.githubLink,
-        githubBranch=request_body.githubBranch
-    )
+    jenkins_job_parameters = generateJenkinsJobParameters(request_body)
+    triggerJenkinsJobOrRaiseException(jenkins_client, jenkins_job_name, jenkins_job_parameters.__dict__)
 
+
+def triggerJenkinsJobOrRaiseException(jenkins_client, jenkins_job_name: str, jenkins_job_parameters: dict) -> int:
+    """jenkins job 요청"""
     try:
         jenkins_queue_id: int = jenkins_client.build_job(
             jenkins_job_name,
             parameters=jenkins_job_parameters.__dict__,
             token=None
         )
+        return jenkins_queue_id
     except jenkins.NotFoundException as e:
         raise RuntimeError("Jenkins job이 존재하지 않습니다.")
     except Exception as e:
-        print(e)
         raise RuntimeError(e)
 
-    print(jenkins_queue_id)
+
+def generateJenkinsJobParameters(request_body):
+    """젠킨스 잡 파라미터 생성"""
+    return CreateJenkinsJobParameters(
+        applicationName=request_body.applicationName,
+        templateVersion=request_body.templateVersion,
+        githubLink=request_body.githubLink,
+        githubBranch=request_body.githubBranch
+    )
 
 
 def getJenkinsJobByTemplateType(template_type: str):
