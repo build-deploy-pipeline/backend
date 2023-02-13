63cb5c0612b8d675ea2671407b8792dc732840bd
choisungwook <kgg1959@naver.com>
2023-02-13 22:38:53 +0900

    웹앱 생성 body default 값 추가

--- app/api/models/webapp.py
+++ app/api/models/webapp.py
@@ -1,10 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class RequestBodyCreateWebApp(BaseModel):
     """웹앱 생성 요청"""
-    applicationName: str
-    templateType: str
-    templateVersion: str
-    githubLink: str
-    githubBranch: str
+    applicationName: str = "demo_application"
+    templateType: str = "fastapi"
+    # templateVersion: str = "0.1"
+    githubLink: str = "https://github.com/build-deploy-pipeline/argo-manifest-nginx-sample.git"
+    githubBranch: str = "main"
+
+    class Config:
+        anystr_strip_whitespace = True
