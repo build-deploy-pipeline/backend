3fb91b27900dc2e3cb601e95268f47d40d95ca17
choisungwook <kgg1959@naver.com>
2023-02-13 21:35:54 +0900

    logger 추가

---
+++ app/log.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+
+
+def get_logger():
+    """로거 생성"""
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    stream_handler = logging.StreamHandler(sys.stdin)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
