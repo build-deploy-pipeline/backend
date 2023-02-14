4a855146f64239ab2d23cfdb8ab1040a7300f23e
choisungwook <kgg1959@naver.com>
2023-02-14 22:07:21 +0900

    로거레벨 설정

--- app/log.py
+++ app/log.py
@@ -5,12 +5,13 @@ import sys
 def get_logger():
     """로거 생성"""
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
     logger.addHandler(stream_handler)
 
     return logger
